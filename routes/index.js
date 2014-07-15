752745d14381964a9c10b198c21bd891c913b66a
Tomas Charad <tomas.charad@gmail.com>
2014-07-15 14:34:48 -0400

    Added sebastian pic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('../public/index-one-page.html', { title: 'TCIT | Web solutions' });
+  res.render('index-one-page.html');
 });
 
 module.exports = router;
