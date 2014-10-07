e354de0932af99d7c9f47abf1c20ad8f7f94e534
Tomas Charad <tomas.charad@gmail.com>
2014-10-07 15:09:10 -0300

    Activated contact mailer

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,3 +6,30 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-45863581-1', 'auto');
 ga('send', 'pageview');
+
+// $( "#contactForm" ).submit(function( event ) {
+//   // Stop form from submitting normally
+//   event.preventDefault();
+//
+//   submitButton = $("#contact-form-submit");
+//
+//   // Get some values from elements on the page:
+//   var $form = $( this ),
+//     contactName = $form.find( "input[name='name']" ).val();
+//     contactEmail = $form.find( "input[name='email']" ).val();
+//     contactSubject = $form.find( "input[name='subject']" ).val();
+//     contactMessage = $form.find( "input[name='message']" ).val();
+//     url = $form.attr( "action" );
+//
+//   // Send the data using post
+//   var posting = $.post( url, { name: contactName, email: contactEmail, subject: contactSubject, message: contactMessage } );
+//
+//   // Put the results in a div
+//   posting.done(function( data ) {
+//     submitButton.attr("value", "Send another message");
+//   });
+//
+//   posting.fail(function() {
+//     submitButton.attr("value", "Ups we had an problem... Try again!");
+//   });
+// });
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,49 @@ router.get('/', function(req, res) {
   res.render('index-one-page.html');
 });
 
+/* Post contact email */
+router.post('/contact-email', function(req, res) {
+  var nodemailer = require('nodemailer');
+  var smtpTransport = require('nodemailer/node_modules/nodemailer-smtp-transport');
+
+  // create reusable transporter object using SMTP transport/ TCT: Using SMPT transporter
+  var transporter = nodemailer.createTransport(smtpTransport({
+      host: 'smtp.ipage.com',
+      port: 587,
+      ignoreTLS: true,
+      auth: {
+          user: 'no-reply@tcit.cl',
+          pass: 'Gransayaman1'
+      }
+  }));
+
+  // NB! No need to recreate the transporter object. You can use
+  // the same transporter object for all e-mails
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: req.body.name+' <'+req.body.email+'>', // sender address
+      to: 'contact@tcit.cl', // list of receivers
+      subject: req.body.subject, // Subject line
+      text: req.body.body, // plaintext body
+      html: '<b>'+req.body.message+'</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+          res.writeHead(505, {'Content-Type': 'text/plain' });
+          res.write(error);
+      }else{
+          console.log('Message sent: ' + info.response);
+          res.writeHead(200, {'Content-Type': 'text/plain' });
+          res.write(info.response);
+          res.end();
+      }
+  });
+});
+
 /* GET site map. */
 router.get('/sitemap', function(req, res) {
   res.render('sitemap.xml');
